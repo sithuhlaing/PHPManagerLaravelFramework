f3fa80dc56da2629181b643c192294726c7f6f8e
sithu.h <sithu.h@rznet.com>
2017-09-01 10:14:00 +0700

    updated controller

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -10,4 +10,92 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    protected function createdResponse($data)
+    {
+        $response = [
+        'code' => Res::HTTP_CREATED,
+        'status' => 'success',
+        'data' => $data
+        ];
+        return response()->json($response, $response['code']);
+    }
+    
+    protected function showResponse($data)
+    {
+        $response = [
+        'code' => Res::HTTP_OK,
+        'status' => 'success',
+        'data' => $data
+        ];
+        return response()->json($response, $response['code']);
+    }
+    
+    protected function listResponse($data)
+    {
+        $response = [
+        'code' => Res::HTTP_OK,
+        'status' => 'success',
+        'data' => $data
+        ];
+
+        return response()->json($response, $response['code']);
+    }
+    
+    protected function notFoundResponse()
+    {
+        $response = [
+        'code' => Res::HTTP_NOT_FOUND,
+        'status' => 'error',
+        'data' => 'Resource Not Found',
+        'message' => 'Not Found'
+        ];
+        return response()->json($response, $response['code']);
+    }
+    
+    protected function deletedResponse()
+    {
+        $response = [
+        'code' => 200, //204,
+        'status' => 'success',
+        'data' => [],
+        'message' => 'Resource deleted'
+        ];
+        return response()->json($response, $response['code']);
+    }
+    
+    protected function clientErrorResponse($data)
+    {
+        $response = [
+        'code' => Res::HTTP_UNPROCESSABLE_ENTITY,
+        'status' => 'error',
+        'data' => $data,
+        'message' => 'Unprocessable entity'
+        ];
+        return response()->json($response, $response['code']);
+    }
+
+    public function InternalErrorResponse($message)
+    {
+        $response = [
+
+            'status' => 'error',
+            'code' => Res::HTTP_INTERNAL_SERVER_ERROR,
+            'data' => $message,
+            'message' => 'InternalError'
+
+        ];
+        return response()->json($response, $response['code']);
+    }
+
+    public function UnauthorizedResponse($message)
+    {
+        $response = [
+            'status' => 'error',
+            'code' => Res::HTTP_UNAUTHORIZED,
+            'data' => $message,
+            'message' => 'Unauthorize',
+        ];
+        return response()->json($response, $response['code']);
+    }
 }
--- app/Http/Controllers/TodoController.php
+++ app/Http/Controllers/TodoController.php
@@ -8,8 +8,82 @@ use App\Models\Todo;
 
 class TodoController extends Controller
 {
-    public function __invoke()
+    // public function __invoke()
+    // {
+    //     return Todo::all();
+    // }
+    
+    private $todoMananger;
+
+	public function __construct()
     {
-        return Todo::all();
+        $this->todoMananger = Manager::createManger(Manager::TodoManager);
+    } 
+
+    protected function validator(array $data, $rules)
+    {
+
+    	// $messages = array(
+	    //     'required' => 'The :attribute is really really really important.',
+	    //     'same'  => 'The :others must match.'
+	    // );
+	    
+	    $rules = $rules ? $rules : [
+	    	'user_id' => 'required',
+	        'task'    => 'required',
+	        'done'    => 'required',
+	    ];
+
+        return \Validator::make($data, $rules);
     }
+
+    public function getAll()
+	{
+		return $this->listResponse($this->playerManager->all());
+	}
+
+    public function save(Request $request)
+	{
+	    $validator = $this->validator($request->all(), null);
+		
+		if ($validator->fails()) {
+			return $this->clientErrorResponse($validator->messages());
+		}
+
+		try{
+			$player = $this->playerManager->create($request->all());
+			return $this->createdResponse($player);
+		} catch(\Exception $e){
+			return $this->clientErrorResponse($e->getMessage());
+		}
+	}
+
+	public function edit(Request $request, $id)
+	{
+		$validator = $this->validator($request->all(), [
+			'PlayerLogin'    => 'unique:players|max:20',
+		]);
+		
+		if ($validator->fails()) {
+			return $this->clientErrorResponse($validator->messages());
+		}
+
+		try{
+			$this->playerManager->update($request->all(), $id, 'PlayerID');
+			return $this->createdResponse($request->all());
+		}catch(\Exception $e){
+			return $this->clientErrorResponse($e->getMessage());
+		}
+	}
+
+	public function delete($id)
+	{
+		try{
+			$this->playerManager->delete($id);
+			return $this->deletedResponse();
+		}catch(\Exception $e){
+			return $this->clientErrorResponse($e->getMessage());
+		}
+		
+	}
 }
